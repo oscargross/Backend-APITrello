44c62e5fdabac639b8312e5b22b1815b2a2a4d70
oscargross <oscar.gross222@gmail.com>
2021-03-28 17:02:34 -0300

    change cors policy

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,13 @@ const app = express();
 export const InitApp = (routes: any) => {
     app.use(express.json());
     app.use(routes);
-    app.use(cors());
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+        app.use(cors());
+        next();
+    });
 
     app.listen(process.env.PORT || 3333 , () => {
         console.info(`Listenning server`);
